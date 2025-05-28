aa27c33547c64b1167a18eff42cd1813fbe8cd62
Faysal <faysal179038@gmail.com>
2025-05-29 03:29:29 +0600

    final commit- legacy ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	extends: ["next/core-web-vitals"],
+	parserOptions: {
+		ecmaVersion: 2021,
+		sourceType: "module",
+	},
+	rules: {
+		// Add your custom rules here, or leave empty
+	},
+};
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
 				transition={{ duration: 0.6, delay: 0.3 }}
 				className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo"
 			>
-				Hi! I'm Shafiul Alam Faysal{" "}
+				Hi! I&apos;m Shafiul Alam Faysal{" "}
 				<Image src={assets.hand_icon} alt="Waving hand icon" className="w-6" />
 			</motion.h3>
 			<motion.h1
